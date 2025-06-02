a42221b986a6d390b3c091b7027f512b0e430d42
szyymkuus <141359922+szyymkuus@users.noreply.github.com>
2025-06-02 14:53:02 +0200

    Add files via upload

---
+++ installResources.ps1
@@ -0,0 +1,11 @@
+# 1. Biblioteka do konwersji YAML
+pip install pyyaml
+
+# 2. Biblioteka do GUI
+pip install PyQt5
+
+# 3. Biblioteka do konwersji XML
+pip install lxml
+
+# 4. PyInstaller do tworzenia pojedynczego pliku .exe
+pip install pyinstaller
